e1fd4afb9eb4939df3d5e8d56e2e3c46d831e86b
william Zhang <williamzhang@williamdeMacBook-Pro-3.local>
2024-06-10 21:39:09 +0800

    增加方法

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,4 +1,4 @@
-import { wordChange, IWordChangeType } from '../index';
+import { wordChange, IWordChangeType, wordsChange, IWordsChangeType } from '../index';
 
 describe('wordChange', () => {
   it('should return "ABC" for "abc"', () => {
@@ -19,5 +19,21 @@ describe('wordChange', () => {
 
   it('should return "Abc" for "abc"', () => {
     expect(wordChange('', IWordChangeType.Aaa)).toBe('');
+  });
+
+  it('AAA1', () => {
+    expect(wordsChange({ words: 'Favorites and suggestions', type: IWordsChangeType.AllInitialUpper })).toBe('Favorites And Suggestions');
+  });
+
+  it('AAA2', () => {
+    expect(wordsChange({ words: 'Favorites and suggestions', type: IWordsChangeType.AllWordUpper })).toBe('FAVORITES AND SUGGESTIONS');
+  });
+
+  it('AAA3', () => {
+    expect(wordsChange({ words: 'Favorites and suggestions', type: IWordsChangeType.FirstLowerOtherInitialUpper })).toBe('favorites And Suggestions');
+  });
+
+  it('AAA4', () => {
+    expect(wordsChange({ words: 'Favorites', type: IWordsChangeType.FirstLowerOtherInitialUpper })).toBe('favorites');
   });
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+/** 不内置字符判断，需要判断的，自行加 */
+
+import { map } from 'lodash-es';
+
 /** 单词转化类型 */
 export enum IWordChangeType {
   /** 首字母大写 */
@@ -19,4 +23,32 @@ export function wordChange(word: string, type: IWordChangeType): string {
     default:
       return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
   }
+}
+
+
+/** 单词转化类型 */
+export enum IWordsChangeType {
+  /** 第一个单词小写，其他的单词首字母大写 */
+  FirstLowerOtherInitialUpper = 1,
+  /** 全部单词首字母大写 */
+  AllInitialUpper,
+  /** 每个单词都大写 */
+  AllWordUpper,
+}
+
+/** 句子或词组转化 */
+export function wordsChange({ words, type }: { words: string, type: IWordsChangeType }): string {
+  if (words.length > 0) {
+    const splitStr = ' ';
+    const wordArr = words.split(splitStr);
+    switch (type) {
+      case IWordsChangeType.AllInitialUpper:
+        return wordArr.map(word => wordChange(word, IWordChangeType.Aaa)).join(splitStr);
+      case IWordsChangeType.AllWordUpper:
+        return wordArr.map(word => wordChange(word, IWordChangeType.AAA)).join(splitStr);
+      default:
+        return map(wordArr, (word, index) => wordChange(word, index === 0 ? IWordChangeType.aaa : IWordChangeType.Aaa)).join(splitStr);
+    }
+  }
+  return ''
 }
